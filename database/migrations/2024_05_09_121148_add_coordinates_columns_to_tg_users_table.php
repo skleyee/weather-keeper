62c8f84570421f4c7745731af84e66a65a73b2de
Sasha <a.kleutin@msocialproduction.com>
2024-05-09 16:25:11 +0300

    migrations

---
+++ database/migrations/2024_05_09_121148_add_coordinates_columns_to_tg_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tg_users', function (Blueprint $table) {
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tg_users', function (Blueprint $table) {
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+        });
+    }
+};
